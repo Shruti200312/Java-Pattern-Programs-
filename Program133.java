d514550bd9d39da2312b26ec3184ffc0021cdbb8
Shruti200312 <shrutijawale.sit.comp@gmail.com>
2024-12-12 19:05:56 +0530

    Add files via upload

---
+++ Program133.java
@@ -0,0 +1,29 @@
+/*
+    O
+   NM
+  LKJ
+ IHGF
+EDCBA
+*/
+
+class Program133
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=(n*(n+1))/2;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print((char)(count+64));
+				count--;
+			}
+			System.out.println();
+		}
+	}
+}
